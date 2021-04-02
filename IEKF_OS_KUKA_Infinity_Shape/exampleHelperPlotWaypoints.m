9eef68bf3fc1b943aab7e4e330a21bc502aecc03
Daan de Kanter <daandekanter1996@gmail.com>
2021-04-02 14:38:49 +0200

    Final code for infinity loop reconstruction

---
+++ IEKF_OS_KUKA_Infinity_Shape/exampleHelperPlotWaypoints.m
@@ -0,0 +1,6 @@
+function exampleHelperPlotWaypoints(wayPoints)
+% This function plots spheres as points of interest
+% Copyright 2017-2018 The MathWorks, Inc.
+for idx = 1:size(wayPoints,1)
+   exampleHelperPlotSpheres(0.01,wayPoints(idx,:));
+end
